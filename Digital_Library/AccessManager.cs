7a63a058e5b9238f50f077539cb7cc714e8b4005
Purpururi <ppoluska@gmail.com>
2026-07-10 19:44:40 +0300

    Add files via upload

---
+++ Digital_Library/AccessManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digital_Library
+{
+    internal class AccessManager
+    {
+        private static readonly Dictionary<string, (string Password, Dictionary<string, string> Permissions)> _users = new()
+    {
+        { "Преподаватель", ("prof", new() { ["author"] = "CRUD", ["publication"] = "CRUD", ["publisher"] = "CRUD" }) },
+        { "Студент", ("stud", new() { ["author"] = "R", ["publication"] = "R", ["publisher"] = "R" }) }
+    };
+
+        public static bool CheckPassword(string role, string password) =>
+            _users.ContainsKey(role) && _users[role].Password == password;
+
+        public static Dictionary<string, string> GetPermissions(string role) =>
+            _users.ContainsKey(role) ? _users[role].Permissions : new();
+    }
+}
